8b78dd3d288cc27e705680861a662aba79d20fbd
gmoissey <gmoisseyev2@huskers.unl.edu>
2023-03-28 15:46:10 -0500

    Trying to add base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite'
 
 export default defineConfig({
+	base: '/countdown-timer',
 	plugins: [sveltekit()],
 	server: {
 		fs: {
